1b5c3ce231551eb197e43f3671615041879f52d3
[dyad] <federicocordioli1974@gmail.com>
2026-01-22 20:12:33 +0100

    [dyad] wrote 1 file(s)

---
+++ src/components/relief/reliefStl.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const ReliefStl: React.FC = () => {
+  return (
+    <div>
+      {/* Add your STL rendering logic here */}
+      <p>STL Component</p>
+    </div>
+  );
+};
+
+export default ReliefStl;
