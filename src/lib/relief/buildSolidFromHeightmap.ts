db5392382b3983fa1ce970475d9abe54c31bacc0
[dyad] <federicocordioli1974@gmail.com>
2026-01-23 18:54:16 +0100

    Updated src/lib/relief/buildSolidFromHeightmap.ts

--- src/lib/relief/buildSolidFromHeightmap.ts
+++ src/lib/relief/buildSolidFromHeightmap.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import type { OutputMode, BaseStyle } from "@/lib/reliefModeTypes";
+import type { OutputMode, BaseStyle } from "@/lib/reliefTypes";
 
 type BuildSolidArgs = {
   normF32: Float32Array;
