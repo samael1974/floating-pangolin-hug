8552816ecc77bcad114c24f40808c7a05f9859c9
[dyad] <federicocordioli1974@gmail.com>
2026-01-24 09:23:30 +0100

    Updated src/components/relief/ReliefWizard.tsx

--- src/components/relief/ReliefWizard.tsx
+++ src/components/relief/ReliefWizard.tsx
@@ -298,7 +298,6 @@ React.useEffect(() => {
     });
   }
 }
-
   return (
     <div className="mx-auto w-full max-w-7xl px-4 pb-10 pt-4">
       <div className="grid gap-6 md:grid-cols-[420px_1fr] lg:grid-cols-[460px_1fr]">
