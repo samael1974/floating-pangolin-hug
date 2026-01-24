0a4555ff99799e0ebdc65b9e30c08a98b646eb08
[dyad] <federicocordioli1974@gmail.com>
2026-01-24 08:21:03 +0100

    Updated src/lib/relief/decodeDepthmapPng.ts

--- src/lib/relief/decodeDepthmapPng.ts
+++ src/lib/relief/decodeDepthmapPng.ts
@@ -1,3 +1,18 @@
-export const decodeDepthmapPng = (pngData: Uint8Array) => {
-  // Implementation of decodeDepthmapPng goes here
+// src/lib/relief/decodeDepthmapPng.ts
+
+export type DecodedDepthmap = {
+  normF32: Float32Array;
+  w: number;
+  h: number;
+  bitDepth?: 8 | 16;
 };
+
+// IMPORTANT: input is Uint8Array
+export function decodeDepthmapPng(bytes: Uint8Array): DecodedDepthmap {
+  // TODO: qui dentro deve esserci il tuo parser PNG vero (IHDR/IDAT/Inflate ecc.)
+  // In questa patch mettiamo almeno un errore esplicito se il parser non è implementato,
+  // così TS non vede più "void".
+  throw new Error(
+    "decodeDepthmapPng: parser non implementato o non collegato. Deve ritornare {normF32,w,h}."
+  );
+}
