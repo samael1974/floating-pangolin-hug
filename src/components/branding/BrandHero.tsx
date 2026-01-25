d771b191ac17ffc74e1ca448006c271016d7371c
[dyad] <federicocordioli1974@gmail.com>
2026-01-25 10:15:09 +0100

    [dyad] Creating the missing BrandHero component - wrote 2 file(s)

---
+++ src/components/branding/BrandHero.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const BrandHero = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-lg">
+      <h1>Welcome to Our Relief Wizard</h1>
+      <p>Your one-stop solution for relief.</p>
+    </div>
+  );
+};
+
+export default BrandHero;
