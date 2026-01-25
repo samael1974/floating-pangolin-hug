015d2c1faac002730422724ff0dafe1a45503b89
[dyad] <federicocordioli1974@gmail.com>
2026-01-25 14:04:04 +0100

    Updated src/components/branding/BrandHero.tsx

--- src/components/branding/BrandHero.tsx
+++ src/components/branding/BrandHero.tsx
@@ -23,7 +23,7 @@ export default function BrandHero({ showBackLink = false }: BrandHeroProps) {
                 aria-hidden="true"
               >
                 <img
-                  src="/home/reliefforge-logo.svg"
+                  src={`${import.meta.env.BASE_URL}home/reliefforge-logo.svg`}
                   alt=""
                   className="h-7 w-7"
                   style={{ color: "#1F4E5F" }}
