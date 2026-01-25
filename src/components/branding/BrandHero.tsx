9d6750671a33b0e98bf162e7fdc5548c6f3ad2ce
[dyad] <federicocordioli1974@gmail.com>
2026-01-25 17:50:56 +0100

    Updated src/components/branding/BrandHero.tsx

--- src/components/branding/BrandHero.tsx
+++ src/components/branding/BrandHero.tsx
@@ -26,7 +26,7 @@ export default function BrandHero({ showBackLink = false }: BrandHeroProps) {
   <img
   src="/home/reliefforge-logo.webp"
   alt="ReliefForge"
-  className="h-7 w-5"
+  className="h-7 w-4.5"
   loading="eager"
 />
 
