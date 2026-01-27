c75e3b9efd2701f8753ae9fcfec88015ceb824e5
[dyad] <federicocordioli1974@gmail.com>
2026-01-27 19:09:59 +0100

    Updated src/components/relief/ReliefPreview3D.tsx

--- src/components/relief/ReliefPreview3D.tsx
+++ src/components/relief/ReliefPreview3D.tsx
@@ -1,53 +1,26 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-
-type Props = {
-  hmState: any; // per ora lo teniamo così (poi lo tipizziamo meglio)
+export type HeightmapState = {
+  normF32: Float32Array;
+  w: number;
+  h: number;
 };
 
-export default function ReliefPreview3D({ hmState }: Props) {
-  // Se vedi questo in console, il componente è montato correttamente
-  console.log("ReliefPreview3D mounted", { hmState });
+type Props = {
+  hmState: HeightmapState | null;
+  stlWidthMm: number;
+  decimateStep: number;
+  depthMm: number;
+  baseMm: number;
+  outputMode?: OutputMode;
+  baseStyle: BaseStyle;
+};
 
-  // Helpers (assi + griglia) nella preview: tienili ON per ora
-  const showHelpers = true;
-  const reliefGeometry =
-  (hmState as any)?.reliefGeometry ??
-  (hmState as any)?.geometry ??
-  (hmState as any)?.meshGeometry ??
-  null;
-
-
-  return (
-    <div style={{ width: "100%", height: 420, background: "#fff" }}>
-      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-        {/* Luci base */}
-        <ambientLight intensity={1} />
-        <directionalLight position={[3, 3, 3]} intensity={1} />
-
-        {/* Assi + griglia (solo preview) */}
-        {showHelpers && (
-          <>
-            <axesHelper args={[50]} />
-            <gridHelper args={[200, 20]} />
-          </>
-        )}
-
-        {/* Cubo di debug: deve essere sempre visibile */}
-        <mesh>
-        {reliefGeometry && (
-  <mesh geometry={reliefGeometry} rotation={[-Math.PI / 2, 0, 0]}>
-    <meshStandardMaterial wireframe />
-  </mesh>
-)}
-
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial />
-        </mesh>
-
-        {/* Controlli mouse */}
-        <OrbitControls makeDefault />
-      </Canvas>
-    </div>
-  );
-}
+export default function ReliefPreview3D(props: Props): JSX.Element | null {
+  const {
+    hmState,
+    stlWidthMm,
+    decimateStep,
+    depthMm,
+    baseMm,
+    outputMode = "relief",
+    baseStyle,
+  } = props;
