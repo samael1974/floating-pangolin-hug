7f38d29f4ffa0e482e3fdbe7dac82cd0e9db1cba
[dyad] <federicocordioli1974@gmail.com>
2026-01-27 19:05:26 +0100

    Updated src/components/relief/ReliefPreview3D.tsx

--- src/components/relief/ReliefPreview3D.tsx
+++ src/components/relief/ReliefPreview3D.tsx
@@ -1,30 +1,39 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-const showHelpers = true;
 
 type Props = {
-  // metti i tuoi props veri qui, anche se non li usi nel debug
-  hmState: any;
+  hmState: any; // per ora lo teniamo così (poi lo tipizziamo meglio)
 };
 
 export default function ReliefPreview3D({ hmState }: Props) {
-  // LOG: se questo non stampa, il componente non viene montato
+  // Se vedi questo in console, il componente è montato correttamente
   console.log("ReliefPreview3D mounted", { hmState });
+
+  // Helpers (assi + griglia) nella preview: tienili ON per ora
+  const showHelpers = true;
 
   return (
     <div style={{ width: "100%", height: 420, background: "#fff" }}>
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+        {/* Luci base */}
         <ambientLight intensity={1} />
         <directionalLight position={[3, 3, 3]} intensity={1} />
-        <axesHelper args={[50]} />
-        <gridHelper args={[200, 20]} />
 
-        {/* Debug object ALWAYS visible */}
+        {/* Assi + griglia (solo preview) */}
+        {showHelpers && (
+          <>
+            <axesHelper args={[50]} />
+            <gridHelper args={[200, 20]} />
+          </>
+        )}
+
+        {/* Cubo di debug: deve essere sempre visibile */}
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial />
         </mesh>
 
+        {/* Controlli mouse */}
         <OrbitControls makeDefault />
       </Canvas>
     </div>
