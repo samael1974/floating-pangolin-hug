ee4e0f8de1df9d8c8e57709f3ada847f201ceefa
[dyad] <federicocordioli1974@gmail.com>
2026-01-29 14:12:21 +0100

    Updated src/components/relief/ReliefPreview3D.tsx

--- src/components/relief/ReliefPreview3D.tsx
+++ src/components/relief/ReliefPreview3D.tsx
@@ -265,6 +265,7 @@ export default function ReliefPreview3D({
           )}
 
          <mesh
+  <mesh
   geometry={solidGeometry}
   position={[0, reliefBaseY, 0]}
   rotation={PREVIEW_MIRROR_Y_180 ? [0, Math.PI, 0] : [0, 0, 0]}
